fbf9ba41f90e85efd2e247498a5143883423eaf6
Bane.Shi <bane.shi@tradeshift.com>
2019-03-12 14:39:48 +0800

    []

--- scripts/etcd.sh
+++ scripts/etcd.sh
@@ -10,25 +10,25 @@ CERTS_DIR=/opt/kubernetes/certs
 
 cat <<EOF >/opt/kubernetes/cfg/etcd.conf
 # [member]
-ETCD_NAME="${ETCD_NAME}"
-ETCD_DATA_DIR="${etcd_data_dir}/default.etcd"
+ETCD_NAME="--name=${ETCD_NAME}"
+ETCD_DATA_DIR="--data-dir=${etcd_data_dir}/default.etcd"
 #ETCD_SNAPSHOT_COUNTER="10000"
 #ETCD_HEARTBEAT_INTERVAL="100"
 #ETCD_ELECTION_TIMEOUT="1000"
-ETCD_LISTEN_PEER_URLS="https://${ETCD_LISTEN_IP}:2380"
-ETCD_LISTEN_CLIENT_URLS="https://${ETCD_LISTEN_IP}:2379,https://127.0.0.1:2379"
+ETCD_LISTEN_PEER_URLS="--listen-peer-urls=https://${ETCD_LISTEN_IP}:2380"
+ETCD_LISTEN_CLIENT_URLS="--listen-client-urls=https://${ETCD_LISTEN_IP}:2379,https://127.0.0.1:2379"
 #ETCD_MAX_SNAPSHOTS="5"
 #ETCD_MAX_WALS="5"
 #ETCD_CORS=""
 #
 #[cluster]
-ETCD_INITIAL_ADVERTISE_PEER_URLS="https://${ETCD_LISTEN_IP}:2380"
+ETCD_INITIAL_ADVERTISE_PEER_URLS="--initial-advertise-peer-urls=https://${ETCD_LISTEN_IP}:2380"
 # if you use different ETCD_NAME (e.g. test),
 # set ETCD_INITIAL_CLUSTER value for this name, i.e. "test=http://..."
-ETCD_INITIAL_CLUSTER="${ETCD_INITIAL_CLUSTER}"
-ETCD_INITIAL_CLUSTER_STATE="new"
-ETCD_INITIAL_CLUSTER_TOKEN="k8s-etcd-cluster"
-ETCD_ADVERTISE_CLIENT_URLS="https://${ETCD_LISTEN_IP}:2379"
+ETCD_INITIAL_CLUSTER="--initial-cluster=${ETCD_INITIAL_CLUSTER}"
+ETCD_INITIAL_CLUSTER_STATE="--initial-cluster-state=new"
+ETCD_INITIAL_CLUSTER_TOKEN="--initial-cluster-token=k8s-etcd-cluster"
+ETCD_ADVERTISE_CLIENT_URLS="--advertise-client-urls=https://${ETCD_LISTEN_IP}:2379"
 #ETCD_DISCOVERY=""
 #ETCD_DISCOVERY_SRV=""
 #ETCD_DISCOVERY_FALLBACK="proxy"
@@ -39,31 +39,29 @@ ETCD_ADVERTISE_CLIENT_URLS="https://${ETCD_LISTEN_IP}:2379"
 #
 #[security]
 CLIENT_CERT_AUTH="true"
-ETCD_CA_FILE="${CERTS_DIR}/ca.pem"
-ETCD_CERT_FILE="${CERTS_DIR}/kubernetes.pem"
-ETCD_KEY_FILE="${CERTS_DIR}/kubernetes-key.pem"
+ETCD_CA_FILE="--trusted-ca-file=${CERTS_DIR}/ca.pem"
+ETCD_CERT_FILE="--cert-file=${CERTS_DIR}/kubernetes.pem"
+ETCD_KEY_FILE="--key-file=${CERTS_DIR}/kubernetes-key.pem"
 PEER_CLIENT_CERT_AUTH="true"
-ETCD_PEER_CA_FILE="${CERTS_DIR}/ca.pem"
-ETCD_PEER_CERT_FILE="${CERTS_DIR}/kubernetes.pem"
-ETCD_PEER_KEY_FILE="${CERTS_DIR}/kubernetes-key.pem"
+ETCD_PEER_CA_FILE="--peer-trusted-ca-file=${CERTS_DIR}/ca.pem"
+ETCD_PEER_CERT_FILE="--peer-cert-file=${CERTS_DIR}/kubernetes.pem"
+ETCD_PEER_KEY_FILE="--peer-key-file=${CERTS_DIR}/kubernetes-key.pem"
 EOF
 
-ETCD_OPS="
---name=\${ETCD_NAME} \\
---data-dir=\${ETCD_DATA_DIR} \\
---listen-peer-urls=\${ETCD_LISTEN_PEER_URLS} \\
---listen-client-urls=\${ETCD_LISTEN_CLIENT_URLS} \\
---initial-advertise-peer-urls=\${ETCD_INITIAL_ADVERTISE_PEER_URLS} \\
---initial-cluster=\${ETCD_INITIAL_CLUSTER} \\
---initial-cluster-token=\${ETCD_INITIAL_CLUSTER_TOKEN} \\
---initial-cluster-state=\${ETCD_INITIAL_CLUSTER_STATE} \\
---trusted-ca-file=\${ETCD_CA_FILE} \\
---cert-file=\${ETCD_CERT_FILE}    \\
---key-file=\${ETCD_KEY_FILE} \\
---peer-trusted-ca-file=\${ETCD_PEER_CA_FILE} \\
---peer-cert-file=\${ETCD_PEER_CERT_FILE} \\
---peer-key-file==\${ETCD_PEER_KEY_FILE} \\
-"
+ETCD_OPS="  \${ETCD_NAME}                         \\
+            \${ETCD_DATA_DIR}                     \\
+            \${ETCD_LISTEN_PEER_URLS}             \\
+            \${ETCD_LISTEN_CLIENT_URLS}           \\
+            \${ETCD_INITIAL_ADVERTISE_PEER_URLS}  \\
+            \${ETCD_INITIAL_CLUSTER}              \\
+            \${ETCD_INITIAL_CLUSTER_STATE}        \\
+            \${ETCD_INITIAL_CLUSTER_TOKEN}        \\
+            \${ETCD_ADVERTISE_CLIENT_URLS}        \\
+            \${ETCD_CA_FILE}                      \\
+            \${ETCD_CERT_FILE}                    \\
+            \${ETCD_KEY_FILE}                     \\
+            \${ETCD_PEER_CA_FILE}                 \\
+            \${ETCD_PEER_KEY_FILE}"
 
 cat <<EOF >/usr/lib/systemd/system/etcd.service
 [Unit]
@@ -83,3 +81,4 @@ EOF
 systemctl daemon-reload
 systemctl enable etcd
 systemctl restart etcd
+systemctl status etcd
--- scripts/make-certs.sh
+++ scripts/make-certs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# 创建证书
+# 创建证书,根据自己的信息酌情修改certs-templete 下面的内容
 #可以自己在本地创建好了，再上传到服务器上，忽略本步骤
 
 yum install cfssl -y
@@ -10,7 +10,8 @@ CERTS_DIR=/opt/kubernetes/certs
 #生成ca
 cfssl gencert -initca ${CERTS_TEMPLATE_DIR}/ca-csr.json | cfssljson -bare ${CERTS_DIR}/ca
 
-#kubernetes
+#kubernetes 
+#需要修改 kubernetes-csr.json 中的etcd 集群ip 以及service-cluster-ip-range 的第一个ip
 cfssl gencert -ca=${CERTS_DIR}/ca.pem -ca-key=${CERTS_DIR}/ca-key.pem -config=${CERTS_TEMPLATE_DIR}/ca-config.json -profile=kubernetes ${CERTS_TEMPLATE_DIR}/kubernetes-csr.json | cfssljson -bare ${CERTS_DIR}/kubernetes
 
 #admin
