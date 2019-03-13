1e1cb522e932f99de0738ef3947ed116250170c2
Bane.Shi <bane.shi@tradeshift.com>
2019-03-13 17:46:49 +0800

    []

---
+++ scripts/dashboard.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#模板地址 https://github.com/kubernetes/kubernetes/tree/release-1.13/cluster/addons/dashboard
+
+/opt/kubernetes/bin/kubectl apply -f /opt/kubernetes/yaml/dashboard/*.yaml
